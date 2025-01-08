635e46aca13e9f8addb0129cd2533ab9dd230eec
Lorenz Hübschle <lorenz@firebolt.io>
2025-01-08 17:50:48 +0100

    Fix sanitizer builds

--- cpp/src/arrow/memory_pool.cc
+++ cpp/src/arrow/memory_pool.cc
@@ -440,12 +440,17 @@ class FireboltAllocator {
   }
 
   static void ReleaseUnused() {
-    // the FireboltAllocator uses jemalloc under the hood, so do the same as
-    // JemallocAllocator::ReleaseUnused
-#ifndef ARROW_JEMALLOC
-    static_assert(false, "FireboltAllocator requires ARROW_JEMALLOC");
-#endif
+#ifdef ARROW_JEMALLOC
+    // the FireboltAllocator uses jemalloc under the hood (except in sanitizer
+    // builds, so check the flag) -> just call JemallocAllocator::ReleaseUnused
     memory_pool::internal::JemallocAllocator::ReleaseUnused();
+#else
+#ifdef __GLIBC__
+    // The return value of malloc_trim is not an error but to inform
+    // you if memory was actually released or not, which we do not care about here
+    ARROW_UNUSED(malloc_trim(0));
+#endif
+#endif
   }
 
   static void PrintStats() { }
